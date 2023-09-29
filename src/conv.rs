4e6fc5d9ff5bdb1a11d4aa35656bf728b346cc8c
Fabrice Desclaux <fabrice.desclaux@cea.fr>
2023-09-29 10:44:21 +0200

    Skip conv

--- src/conv.rs
+++ src/conv.rs
@@ -103,7 +103,7 @@ pub(crate) unsafe extern "C" fn converse<C: Conversation>(
     for i in 0..num_msg as isize {
         // get indexed values
         // FIXME: check this
-        let m: &PamMessage = *(msg.offset(i)).as_ref();
+        let m: &PamMessage = (*(msg.offset(i))).as_ref();
         let r: &mut PamResponse = &mut *(resp.offset(i));
 
         let msg = CStr::from_ptr(m.msg);
